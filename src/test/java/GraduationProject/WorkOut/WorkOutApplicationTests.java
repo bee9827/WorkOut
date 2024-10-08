6ae383ca6ac56cc672dcf4c3dfda8e1939fa6007
bee9827 <ehfrhfo9494@naver.com>
2024-10-08 13:56:24 +0900

    Initial commit

---
+++ src/test/java/GraduationProject/WorkOut/WorkOutApplicationTests.java
@@ -0,0 +1,13 @@
+package GraduationProject.WorkOut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
