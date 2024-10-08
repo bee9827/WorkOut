d1fdad3550d8906ccc9c90672f7f24dcbb48c2c3
bee9827 <ehfrhfo9494@naver.com>
2024-10-08 22:02:46 +0900

    exercise 삭제

--- src/main/java/GraduationProject/WorkOut/controller/ExerciseController.java
+++ src/main/java/GraduationProject/WorkOut/controller/ExerciseController.java
@@ -6,18 +6,21 @@ import GraduationProject.WorkOut.repository.ExerciseRepository;
 import GraduationProject.WorkOut.service.ExerciseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ExerciseController {
     private final ExerciseService exerciseService;
+    private final ExerciseRepository exerciseRepository;
 
     @GetMapping("api/exercise")
     public ResponseEntity<ExerciseResponseDto> getExercises(@RequestBody ExerciseRequestDto exerciseRequestDto) {
         return ResponseEntity.ok(exerciseService.findAll(exerciseRequestDto));
     }
-
+    @DeleteMapping("api/exercise")
+    public ResponseEntity<Void> deleteExercise(@RequestParam Integer exercise_id) {
+        exerciseRepository.deleteById(exercise_id);
+        return ResponseEntity.ok().build();
+    }
 }
