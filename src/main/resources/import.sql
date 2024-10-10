abbbc081ee201096b1a5276e92f8166f17358b17
bee9827 <ehfrhfo9494@naver.com>
2024-10-10 13:44:19 +0900

    build: import.sql
    시작데이터 삽입

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into type(type_id, name) values (1,'PUSH_UP');
+insert into type(type_id, name) values (2,'PULL_UP');
+insert into type(type_id, name) values (3,'SQUAT');
+insert into member(member_id, name, email, phone, password, created_date, updated_date, status) values (1,'Yong', 'ehfrhfo9494@naver.com','010-7111-7499','1007','2024-10-09T00:00:00','2024-10-09T00:00:10','Login')
